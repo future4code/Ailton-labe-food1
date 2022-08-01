96b644042d1791f82cf6ed7e9822c72b694cbfd6
Lincoln Ribeiro de Paula <lincoln20012@outlook.com>
2022-08-01 14:57:47 -0300

    login/signup

---
+++ labefood-rappi4/src/pages/LoginPage/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import {primaryColor, secondColor} from './../../constants/colors/colors'
+
+export const DivContainer = styled.div`
+
+background-color: ${primaryColor};
+min-height: 100vh;
+`
+
+export const Form = styled.form`
+
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+width: 50%;
+`
