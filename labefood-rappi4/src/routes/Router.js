b1829ed0a7d12d367dc23419b40d2847d4093c69
savio henriques mourao ayres <savioayres@hotmail.com>
2022-08-01 17:53:41 -0300

    Implementação da página de endereço de cadastro

--- labefood-rappi4/src/routes/Router.js
+++ labefood-rappi4/src/routes/Router.js
@@ -1,39 +1,36 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUpPage from './../pages/SignupPage/SignupPage'
 import LoginPage from './../pages/LoginPage/LoginPage'
+import AddressRegistrationFormPage from './../pages/AddressRegistrationFormPage/AddressRegistrationFormPage'
 import FeedPage from './../pages/FeedPage/FeedPage'
 import ProfilePage from './../pages/ProfilePage/ProfilePage'
 import EditInfoPage from './../pages/ProfilePage/EditInfoPage/EditInfoPage'
-import EditEndPage from './../pages/ProfilePage/EditEndePage/EditEndePage';
+import EditEndPage from './../pages/ProfilePage/EditEndePage/EditEndePage'
 import CartPage from './../pages/CartPage/CartPage'
 import DetailRestPage from './../pages/DetailRestPage/DetailRestPage'
 import ErrorPage from './../pages/ErrorPage/ErrorPage'
 
-const Router = () =>{
-
-return(
+const Router = () => {
+  return (
     <BrowserRouter>
-
-        <Routes>
-            
-    <Route path='/login' element={<LoginPage/>}></Route>
-    <Route path='/signup' element={<SignUpPage/>}></Route>
-    <Route path='/' element={<FeedPage/>}></Route>
-    <Route path='profile' element={<ProfilePage/>}></Route>
-    <Route path='/edit/inf' element={<EditInfoPage/>}></Route>
-    <Route path='/edit/end' element={<EditEndPage/>}></Route>
-    <Route path='/cart' element={<CartPage/>}></Route>
-    <Route path='/res/:id' element={<DetailRestPage/>}></Route>
-    <Route path='*' element={<ErrorPage/>}></Route>
-
-        </Routes>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route
+          path="/address"
+          element={<AddressRegistrationFormPage />}
+        ></Route>
+        <Route path="/" element={<FeedPage />}></Route>
+        <Route path="profile" element={<ProfilePage />}></Route>
+        <Route path="/edit/inf" element={<EditInfoPage />}></Route>
+        <Route path="/edit/end" element={<EditEndPage />}></Route>
+        <Route path="/cart" element={<CartPage />}></Route>
+        <Route path="/res/:id" element={<DetailRestPage />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
     </BrowserRouter>
-
-)
-
-
-
+  )
 }
 
-export default Router 
+export default Router
--- labefood-rappi4/src/services/user.js
+++ labefood-rappi4/src/services/user.js
@@ -1,43 +1,54 @@
-import React from "react";
-import axios from "axios";
-import { Navigate } from "react-router-dom";
-import {BASE_URL} from './../constants/Url/url'
-import goToPage from "../routes/coordinator";
-
+import React from 'react'
+import axios from 'axios'
+import { Navigate } from 'react-router-dom'
+import { BASE_URL } from './../constants/Url/url'
+import goToPage from '../routes/coordinator'
 
 export const login = (body, clear, Navigate, setIsLoading) => {
-
-      axios
-        .post(`${BASE_URL}/login`, body)
-        .then((res) => {
-            console.log(res)
-          localStorage.setItem("token", res.data.token)
-          clear()
-          setIsLoading(false)
-          goToPage(Navigate, "")
-
-        })
-        .catch((err) => {
-          setIsLoading(false)
-          alert(err);
-        });
-
-    };
-
+  axios
+    .post(`${BASE_URL}/login`, body)
+    .then((res) => {
+      console.log(res)
+      localStorage.setItem('token', res.data.token)
+      clear()
+      setIsLoading(false)
+      goToPage(Navigate, '')
+    })
+    .catch((err) => {
+      setIsLoading(false)
+      alert(err)
+    })
+}
 
 export const signup = (body, clear, Navigate, setIsLoading) => {
+  axios
+    .post(`${BASE_URL}/signup`, body)
+    .then((res) => {
+      clear()
+      setIsLoading(false)
+      goToPage(Navigate, 'login')
+    })
+    .catch((err) => {
+      setIsLoading(false)
+      alert(err)
+    })
+}
 
-        axios
-          .post(`${BASE_URL}/signup`, body)
-          .then((res) => {
-            clear()
-            setIsLoading(false)
-            goToPage(Navigate, "login")
-  
-          })
-          .catch((err) => {
-            setIsLoading(false)
-            alert(err);
-          });
-  
-      };
+export const address = (body, clear, Navigate, setIsLoading) => {
+  axios
+    .put(`${BASE_URL}/address`, body, {
+      headers: {
+        auth: localStorage.getItem('token')
+      }
+    })
+    .then((res) => {
+      console.log(res)
+      setIsLoading(false)
+      clear()
+      goToPage(Navigate, 'address')
+    })
+    .catch((err) => {
+      setIsLoading(false)
+      alert(err)
+    })
+}
