e2f689451898de896455778c98279f6e98bc4dee
savio henriques mourao ayres <savioayres@hotmail.com>
2022-08-02 17:37:39 -0300

    alteração no endereço

--- labefood-rappi4/src/services/user.js
+++ labefood-rappi4/src/services/user.js
@@ -1,17 +1,13 @@
-
 import React from 'react'
 import axios from 'axios'
 import { Navigate } from 'react-router-dom'
 import { BASE_URL } from './../constants/Url/url'
 import goToPage from '../routes/coordinator'
-;
-
 
 export const login = (body, clear, Navigate, setIsLoading) => {
   axios
     .post(`${BASE_URL}/login`, body)
     .then((res) => {
-
       console.log(res)
       localStorage.setItem('token', res.data.token)
       clear()
@@ -23,7 +19,6 @@ export const login = (body, clear, Navigate, setIsLoading) => {
       alert(err)
     })
 }
-
 
 export const address = (body, clear, Navigate, setIsLoading) => {
   axios
@@ -44,31 +39,20 @@ export const address = (body, clear, Navigate, setIsLoading) => {
     })
 }
 
-
 export const signup = (body, clear, Navigate, setIsLoading, verifyPass) => {
-
-      if(body.password === verifyPass){
-
-        axios
-          .post(`${BASE_URL}/signup`, body)
-          .then((res) => {
-            clear()
-            setIsLoading(false)
-            goToPage(Navigate, "login")
-  
-          })
-          .catch((err) => {
-            setIsLoading(false)
-            alert(err);
-          });
-
-      }else{
-
-        return alert('Senhas não coincidem')
-
-      }
-
-      };
-
-
-
+  if (body.password === verifyPass) {
+    axios
+      .post(`${BASE_URL}/signup`, body)
+      .then((res) => {
+        clear()
+        setIsLoading(false)
+        goToPage(Navigate, 'login')
+      })
+      .catch((err) => {
+        setIsLoading(false)
+        alert(err)
+      })
+  } else {
+    return alert('Senhas não coincidem')
+  }
+}
